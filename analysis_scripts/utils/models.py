64d0ffded1067c345ac37a6031a869e2b9719eac
Gustavo R Montes <gustavoribeiromontes@gmail.com>
2025-05-01 22:47:09 -0300

    feat(analysis/utils): add transport modes model

---
+++ analysis_scripts/utils/models.py
@@ -0,0 +1,20 @@
+meios_transporte = {
+    1: "Metrô",
+    2:"Trem",
+    3: "Monotrilho",
+    4: "Ônibus/micro-ônibus/van do município de São Paulo",
+    5: "Ônibus/micro-ônibus/van de outros municípios",
+    6: "Ônibus/micro-ônibus/van metropolitano",
+    7: "Transporte Fretado",
+    8: "Transporte Escolar",
+    9: "Dirigindo Automóvel",
+    10: "Passageiro de Automóvel",
+    11: "Táxi Convencional",
+    12: "Táxi não Convencional / aplicativo",
+    13: "Dirigindo Moto",
+    14: "Passageiro de Moto",
+    15: "Passageiro de Mototáxi",
+    16: "Bicicleta",
+    17: "A Pé",
+    18: "Outros"
+}
